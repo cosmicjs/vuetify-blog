032d25923c8c275d0d2f77b15f229af754cb476b
availit <aaronvail14@gmail.com>
2019-01-15 11:30:43 -0600

    clean up unused code, update package.json

--- src/main.js
+++ src/main.js
@@ -12,12 +12,10 @@ import DateFilter from '@/filters/Date'
 // Global Components
 import Toolbar from '@/components/Navigation/Toolbar'
 import PostDialog from '@/components/Posts/PostDialog'
-import FullWidthSection from '@/components/FullWidthSection'
 import Footer from '@/components/Navigation/Footer'
 
 Vue.component('app-toolbar', Toolbar)
 Vue.component('app-post-dialog', PostDialog)
-Vue.component('FullWidthSection', FullWidthSection)
 Vue.component('app-footer', Footer)
 
 Vue.config.productionTip = false
