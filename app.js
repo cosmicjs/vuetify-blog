88d76b4fec7ff42ac075bda8b2d7136015548668
availit <aaronvail14@gmail.com>
2019-01-19 13:06:33 -0600

    add express index.html redirect

--- app.js
+++ app.js
@@ -3,5 +3,10 @@ const app = express()
 
 app.use(express.static('./dist'));
 
+/* final catch-all route to index.html */
+app.get('/*', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+})
+
 app.listen(process.env.PORT || 5000, function () {
 });
