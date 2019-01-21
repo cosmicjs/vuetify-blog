1acc021fc691e423ed074ac26237571793cbda7a
availit <aaronvail14@gmail.com>
2019-01-21 07:55:48 -0600

    fix express catch all redirect

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 
-app.use(express.static('./dist'));
+app.use(express.static('./dist'))
 
 /* final catch-all route to index.html */
-app.get('/*', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/index.html'))
 })
 
-app.listen(process.env.PORT || 5000, function () {
-});
+const port = process.env.PORT || '5000'
+app.set('port', port)
+
+app.listen(process.env.PORT || 5000, function () {})
